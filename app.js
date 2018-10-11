79f7e9c3a65a00d1b37410a77a0268b1040ca76c
Cathryn Pierce <38726156+Cpierce94@users.noreply.github.com>
2018-10-11 12:19:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 /*
- * Create a list that holds all of your cards
+ * A list that holds all of your cards
  */
+const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", 
+			   "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", 
+			   "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
+const cardsContainer = document.querySelector(".deck");
+
+let openedCards = [];
+let matchedCards = [];
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+*Shuffles the cards
+*/
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +27,129 @@ function shuffle(array) {
     return array;
 }
 
+/*
+*Starts the game
+*/
+function startGame() {
+	for (let i = 0; i < icons.length; i++) {
+		const card = document.createElement("li");
+		card.classList.add("card");
+		card.innerHTML = `<i class="${icons[i]}"></i>`; 
+		cardsContainer.appendChild(card);
+
+		click(card);
+	}
+}
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+*Click Event
+*/
+function click(card) {
+	card.addEventListener("click", function() {
+		const currentCard = this;
+		const previousCard = openedCards[0];
+
+	//Existing opened card
+		if(openedCards.length === 1) {
+			card.classList.add("open", "show", "disable");
+			openedCards.push(this);
+
+ 			compare(currentCard, previousCard);
+
+		} else {
+			card.classList.add("open", "show", "disable");
+			openedCards.push(this);
+		}
+	});
+}
+
+/*
+*Compares the cards
+*/
+function compare(currentCard, previousCard) {
+	if(currentCard.innerHTML === previousCard.innerHTML) {
+		currentCard.classList.add("match");
+		previousCard.classList.add("match");
+
+		matchedCards.push(currentCard, previousCard);
+				
+		openedCards = [];
+
+		isOver();
+
+	} else {
+
+		setTimeout(function () {
+			currentCard.classList.remove("open", "show", "disable");
+			previousCard.classList.remove("open", "show", "disable");
+			openedCards = [];
+			}, 500);	
+	}
+	addMove();
+}
+
+/*
+*Checks if game is over
+*/
+function isOver(){
+	if(matchedCards.length === icons.length) {
+		alert("YOU WIN!");
+	}
+
+}
+
+/*
+*Add Move
+*/
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+function addMove() {
+	moves++;
+	movesContainer.innerHTML = moves;
+
+	//Sets the rating
+	rating();
+}
+
+/*
+*Game Rating
+*/
+const starsContainer = document.querySelector(".stars");
+const star = `<li><i class="fa fa-star"></i></li>`;
+starsContainer.innerHTML = star + star + star;
+
+function rating() {
+	switch(moves) {
+		case 18:
+			starsContainer.innerHTML = star + star;
+		break;
+
+		case 25:
+			starsContainer.innerHTML = star;
+		break;
+	}
+}
+
+/*
+*Restarts the Game
+*/
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function() {
+	//Delete all cards
+	cardsContainer.innerHTML = "";
+
+	//Call startGame to create new cards
+	startGame();
+	
+	//Reset any related variables like matchedCards
+	matchedCards = [];
+
+	moves = 0;
+	movesContainer.innerHTML = 0;
+
+	starsContainer.innerHTML = star + star + star;
+});
+
+//Start the game for the first time
+shuffle(icons);
+startGame();
